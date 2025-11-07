521909c3f507147413d1ffc1cdf6b606d97c21d4
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-11-07 00:22:46 +0000

    trending music card desktop verison created

--- src/components/top-15/Trending15.jsx
+++ src/components/top-15/Trending15.jsx
@@ -4,9 +4,10 @@ import Trending15Card from "./Trending15Card";
 const Trending15 = ({ trending15 }) => {
 
     return (
-        <div className="space-y-6">
+        <div className="space-y-3">
             <h2 className="text-xl font-bold text-primaryText">Trending Top {trending15.length}</h2>
-            <div className="grid grid-cols-3 gap-5">
+            {/* trending songs cards */}
+            <div className="grid lg:grid-cols-3 md:grid-cols-1 grid-cols-1 gap-x-11 gap-y-4">
                 {trending15.map((eachTrendingSong, index) => <Trending15Card index={index} eachTrendingSong={eachTrendingSong}></Trending15Card>)}
             </div>
 
--- src/components/top-15/Trending15Card.jsx
+++ src/components/top-15/Trending15Card.jsx
@@ -1,32 +1,54 @@
+import { useState } from "react";
 import { FaRegHeart } from "react-icons/fa6";
+import { Link } from "react-router";
+import { PiGooglePlayLogo } from "react-icons/pi";
+
 
 
 const Trending15Card = ({ eachTrendingSong, index }) => {
 
     const { title, artist, duration, coverImage } = eachTrendingSong
     const songIndex = index + 1;
+
+    const [MouseEnter, setMouseEnter] = useState(false)
+
+    function MouseEnterHover() {
+        setMouseEnter(true)
+        console.log(MouseEnter)
+    }
+    function MouseLeaveHover() {
+        setMouseEnter(false)
+        console.log(MouseEnter)
+    }
+
+
     return (
-        <div className='flex justify-between items-center '>
+        <Link onMouseEnter={MouseEnterHover} onMouseLeave={MouseLeaveHover} className=" border-b border-gray-600/40 py-3">
+            <div className='flex justify-between items-center'>
+                <div className='flex gap-7 items-center '>
+                    <div className='flex w-20'>
+                        <h1 className={`lg:text-5xl text-3xl font-extrabold  ${MouseEnter ? "text-primaryText transition duration-300 font-bold" : ""}`}>{`${songIndex < 10 ? 0 : ""}${songIndex}`}</h1>
+                    </div>
+                    <div className="relative">
+                        <img className='w-14 h-13 rounded ' src={coverImage} alt="" />
 
-            <div className='flex gap-3 items-center '>
-                <div className='flex w-20'>
-                    <h1 className='text-5xl font-extrabold'>{`${songIndex < 10 ? 0 : ""}${songIndex}`}</h1>
+                        <PiGooglePlayLogo className={`absolute z-10 transition-all duration-300 text-2xl ${MouseEnter ? " top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-100" : "top-5 left-1/2 opacity-0  -translate-y-full -translate-x-1/2 "}`}></PiGooglePlayLogo>
+                        <div className={`bg-black/50 w-full h-full absolute top-0 rounded opacity-0 transition-all duration-300 ${MouseEnter && "opacity-100"}`}>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h4 className={`font-bold lg:text-[16px] md:text-[15px] ${MouseEnter ? "text-primaryText transition duration-500 " : ""} `}>{title}</h4>
+                        <h4 className="md:text-[15px]">{artist}</h4>
+                    </div>
                 </div>
-
-                <img className='w-11 h-12 rounded' src={coverImage} alt="" />
-
-                <div>
-                    <h4 className="font-bold text-[16px]">{title}</h4>
-                    <h4 className="text-[13px]">{artist}</h4>
+                <div className="flex items-center gap-3">
+                    <p className=''>{duration}</p>
+                    <FaRegHeart></FaRegHeart>
                 </div>
             </div>
-            <div className="flex items-center gap-3">
-                <p className=''>{duration}</p>
-                <FaRegHeart></FaRegHeart>
+        </Link >
 
-            </div>
-
-        </div>
     );
 };
 

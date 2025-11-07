77ae870556f60cde9c638f09714a1f7fc30c7d81
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-11-07 00:29:48 +0000

    trending song cart is now resposive for all devices

--- src/components/top-15/Trending15Card.jsx
+++ src/components/top-15/Trending15Card.jsx
@@ -25,12 +25,12 @@ const Trending15Card = ({ eachTrendingSong, index }) => {
     return (
         <Link onMouseEnter={MouseEnterHover} onMouseLeave={MouseLeaveHover} className=" border-b border-gray-600/40 py-3">
             <div className='flex justify-between items-center'>
-                <div className='flex gap-7 items-center '>
-                    <div className='flex w-20'>
+                <div className='flex lg:gap-7 md:gap-5 gap-7 items-center '>
+                    <div className='flex lg:w-20'>
                         <h1 className={`lg:text-5xl text-3xl font-extrabold  ${MouseEnter ? "text-primaryText transition duration-300 font-bold" : ""}`}>{`${songIndex < 10 ? 0 : ""}${songIndex}`}</h1>
                     </div>
                     <div className="relative">
-                        <img className='w-14 h-13 rounded ' src={coverImage} alt="" />
+                        <img className='lg:w-14 lg:h-13  w-13 h-12  rounded ' src={coverImage} alt="" />
 
                         <PiGooglePlayLogo className={`absolute z-10 transition-all duration-300 text-2xl ${MouseEnter ? " top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-100" : "top-5 left-1/2 opacity-0  -translate-y-full -translate-x-1/2 "}`}></PiGooglePlayLogo>
                         <div className={`bg-black/50 w-full h-full absolute top-0 rounded opacity-0 transition-all duration-300 ${MouseEnter && "opacity-100"}`}>
@@ -38,8 +38,8 @@ const Trending15Card = ({ eachTrendingSong, index }) => {
                     </div>
 
                     <div>
-                        <h4 className={`font-bold lg:text-[16px] md:text-[15px] ${MouseEnter ? "text-primaryText transition duration-500 " : ""} `}>{title}</h4>
-                        <h4 className="md:text-[15px]">{artist}</h4>
+                        <h4 className={`font-bold lg:text-[16px] text-[15px] ${MouseEnter ? "text-primaryText transition duration-500 " : ""} `}>{title}</h4>
+                        <h4 className="lg:text-[13px] text-[12px]">{artist}</h4>
                     </div>
                 </div>
                 <div className="flex items-center gap-3">
